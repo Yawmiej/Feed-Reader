4373825110f5d7abffb710314955905f9860eaa0
yawmiej <yomlateef@yahoo.com>
2018-11-20 14:00:16 +0100

    All test written

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,11 +54,8 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function(){
-        let body;
+        let body = document.querySelector('body');;
         
-        beforeEach(function(){
-            body = document.querySelector('body');
-        })
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
